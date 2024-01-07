7e31c3222acc0fdc45e5fcc57f8088c60d3bd863
yonghun16 <yonghun16@naver.com>
2024-01-07 19:54:09 +0900

    25 JS Intersection Observer(요소의 가시성 관찰)

--- js/main.js
+++ js/main.js
@@ -69,3 +69,17 @@ searchStarterEl.addEventListener('click', showSearch)
 searchCloserEl.addEventListener('click', hideSearch)
 searchShadowEl.addEventListener('click', hideSearch)
 
+
+/* 요소의 가시성 관찰 */
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return 
+    }
+    entry.target.classList.add('show')
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
